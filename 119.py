292ce17f2253fac08d87905f20f837eec4ec6633
Scott Boutang <8740452+sboutang@users.noreply.github.com>
2023-12-12 08:14:35 -0600

    Update 119.py

--- 119.py
+++ 119.py
@@ -1,74 +1,45 @@
 #!/usr/bin/env python3
-# def compress_domains(domains):
-#     compressed_parts = []
-#     compressed_string = ""
+import argparse
 
-#     for domain in domains:
-#         parts = domain.split('.')
-#         for part in parts:
-#             length_hex = hex(len(part))[2:].zfill(2)
-#             characters_hex = ''.join([hex(ord(char))[2:].zfill(2) for char in part])
-#             compressed_parts.append(length_hex + characters_hex)
+def convert_domains_to_option_119(domains):
+  compressed_data = bytearray()
+  pointers = {}
 
-#     compressed_string = ''.join(compressed_parts)
+  for domain in domains:
+    parts = domain.lower().split('.')
+    for part in parts:
+      if part in pointers:
+        pointer = pointers[part]
+        compressed_data.extend([0xc0, 0x00 | pointer])
+      else:
+        pointers[part] = len(compressed_data)
+        compressed_data.extend([len(part), *[ord(char) for char in part]])
+    compressed_data.append(0x00)
 
-#     return compressed_string
-def compress_domains(domains):
-    hex_string = ""
-    for domain in domains.split():
-        domain_parts = domain.split('.')
-        for part in domain_parts:
-            hex_string += format(len(part), '02x') + part.encode('utf-8').hex()
-        hex_string += "00"
-    return hex_string
+  return compressed_data.hex().upper()
 
-def compress_with_pointers(domains_list, input_domains):
-    compressed_string = compress_domains(input_domains)
-    print(f'full hex list: {compressed_string}')
-    hex_string = ""
-    compressed_with_pointers = ""
-    offset = 0
+def format_as_windows(hex_string):
+    byte_string = " ".join([hex_string[i:i+2] for i in range(0, len(hex_string), 2)])
+    return byte_string
 
-    for domain in domains_list:
-        parts = domain.split('.')
-        for part in parts:
-            current_part = format(len(part), '02x') + part.encode('utf-8').hex()
-            print(f'{part} in hex is: {current_part}')
-            if current_part in compressed_with_pointers[offset:]:
-                pointer_offset = compressed_with_pointers[offset:].index(current_part)
-                print(f'offset: {pointer_offset}')
-                compressed_with_pointers += current_part
-                print(f'building {compressed_with_pointers}')
-            else:
-                compressed_with_pointers += current_part
-                print(compressed_with_pointers)
-                offset += len(current_part)
-        # for part in parts:
-        #     # print(part)
-        #     length_hex = hex(len(part))[2:].zfill(2)
-        #     characters_hex = ''.join([hex(ord(char))[2:].zfill(2) for char in part])
-        #     current_part = length_hex + characters_hex
-        #     print(f'hex length and value of {part}: {current_part}')
+def format_as_meraki(hex_string):
+    byte_string = ":".join([hex_string[i:i+2] for i in range(0, len(hex_string), 2)])
+    return byte_string
 
-        #     if current_part in compressed_string[offset:]:
-        #         pointer_offset = compressed_string[offset:].index(current_part)
-        #         print(f'pointer_offset: {pointer_offset}')
-        #         pointer_hex = hex(pointer_offset | 0xC000)[2:].zfill(4)
-        #         print(f'pointer_hex: {pointer_hex}')
-        #         compressed_with_pointers += pointer_hex
-        #         offset += pointer_offset + len(current_part)
-        #         print(f'offset: {offset}')
-        #     else:
-        #         compressed_with_pointers += current_part
-        #         offset += len(current_part)
+def format_as_cisco(hex_string):
+    byte_string = ".".join([hex_string[i:i+4] for i in range(0, len(hex_string), 4)])
+    return byte_string
 
-    return compressed_with_pointers
-
-
-# Example usage:
-input_domains = "google.com yahoo.com"
-domains_list = input_domains.split()
-compressed_result = compress_with_pointers(domains_list, input_domains)
-
-print(f"Original domains: {input_domains}")
-print(f"Compressed result: {compressed_result}")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("domain_input", nargs='+', help="Enter the domain name or names as space seperated")
+    args = parser.parse_args()
+    domain_list = ' '.join(args.domain_input)
+    print(f'Domain list: {domain_list}')
+    option_119_hex = convert_domains_to_option_119(args.domain_input)
+    meraki_119_string = format_as_meraki(option_119_hex)
+    windows_119_string = format_as_windows(option_119_hex)
+    cisco_119_string = format_as_cisco(option_119_hex)
+    print(f'Meraki: {meraki_119_string}')
+    print(f'Windows: {windows_119_string}')
+    print(f'Cisco: {cisco_119_string}')
